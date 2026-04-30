1a359cc6fcd813a0c00fedd7899913a6bd022292
gein13 <schamberlain@my365.bellevue.edu>
2026-04-30 17:05:26 -0500

    Pushing 4/30/2026

---
+++ module-7/city_functions.py
@@ -0,0 +1,14 @@
+def city_country(city, country, population=None, language=None):
+    if population and language:
+        return f"{city}, {country}, - Population: {population}, {language}"
+    elif population:
+        return f"{city}, {country}, - Poplation: {population}"
+    else:
+        return f"{city}, {country}"
+
+if __name__ == "__main__":
+# Call the function at least three times
+    print(city_country("Toronto", "Canada"))
+    print(city_country("Gothenburg", "Sweden", 649847))
+    print(city_country("Helsinki", "Finland", 600000, "Finnish"))
+    print(city_country("Minneapolis", "USA", 427499, "English"))
---
+++ module-7/test_cities.py
@@ -0,0 +1,14 @@
+import unittest
+from city_functions import city_country
+
+
+class CitiesTestCase(unittest.TestCase):
+
+    def test_city_country(self):
+        """Test that city and country are formatted correctly."""
+        result = city_country("Toronto", "Canada")
+        self.assertEqual(result, "Toronto, Canada")
+
+
+if __name__ == "__main__":
+    unittest.main()
